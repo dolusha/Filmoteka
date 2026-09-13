9a4bed65fdb533a31cd0f32edd54882c12252cb2
Dmitriy <dimaborovik07@gmail.com>
2026-09-13 18:55:44 +0300

    Initial Commit

---
+++ Filmoteka/FilmotekaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmotekaApp.swift
+//  Filmoteka
+//
+//  Created by mac on 13.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct FilmotekaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
