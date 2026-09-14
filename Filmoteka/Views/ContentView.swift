05febf47178591bba21873ee9ac8eb5ef38c94e3
Dmitriy <dimaborovik07@gmail.com>
2026-09-14 14:47:49 +0300

    feat: add design of main page (ConttentView) for searching Movies

---
+++ Filmoteka/Views/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  Filmoteka
+//
+//  Created by mac on 13.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var movieViewModel = MovieViewModel()
+    
+    private let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                TextField("Search film", text: $movieViewModel.searchText)
+                    .padding()
+                    .textFieldStyle(.roundedBorder)
+                    .onSubmit {
+                        movieViewModel.searchMovies()
+                    }
+            }
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(movieViewModel.movies) { movie in
+                        NavigationLink(destination: MovieDetailView(movie: movie)) {
+                            VStack {
+                                AsyncImage(url: movie.posterUrl) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                } placeholder: {
+                                    Color.gray.opacity(0.2)
+                                }
+                                .frame(height: 250)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                
+                                Text(movie.title)
+                                    .font(.headline)
+                                    .multilineTextAlignment(.center)
+                                    .lineLimit(2)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle("Movies")
+        .onAppear {
+            movieViewModel.loadPopularMovies()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Filmoteka/Views/MovieDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailView.swift
+//  Filmoteka
+//
+//  Created by mac on 14.09.2026.
+//
+
+import Foundation
+import SwiftUI
+
+struct MovieDetailView: View {
+    let movie: Movie
+    var body: some View {
+        
+    }
+}
