ad497c2d5c4ede74b130f9785ebe3f1b28731fef
Dmitriy <dimaborovik07@gmail.com>
2026-09-14 15:01:53 +0300

    feat: add detailedView for films MovieDetailView

--- Filmoteka/Views/MovieDetailView.swift
+++ Filmoteka/Views/MovieDetailView.swift
@@ -11,6 +11,39 @@ import SwiftUI
 struct MovieDetailView: View {
     let movie: Movie
     var body: some View {
-        
+        ScrollView {
+            VStack {
+                AsyncImage(url: movie.posterUrl) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    Color.gray.opacity(0.2)
+                }
+                .cornerRadius(12)
+                .padding()
+                
+                Text(movie.overview)
+                    .padding()
+                Spacer()
+            }
+        }
+        .navigationTitle("Description")
+        .navigationBarTitleDisplayMode(.inline)
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#Preview {
+    
 }
